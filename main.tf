d9ce1cd2fb16ba3e84bfd86f035b25245d6613cf
Arm Bun <bellotaophyc@outlook.com>
2022-08-11 10:09:01 +0000

    change see_algorithm to sse_algorithm

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@
     server_side_encryption_configuration {
         rule {
             apply_server_side_encryption_by_default {
-            see_algorithm = "AES256"
+            sse_algorithm = "AES256"
             }
         }
     }
